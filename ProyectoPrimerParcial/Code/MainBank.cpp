d53b5c0a2fa6b93b650bf9b2e4ccf47ae778210f
Abigail Cabascango <51092317+sacabascango1@users.noreply.github.com>
2020-06-16 18:27:19 -0500

    Files for Project

---
+++ ProyectoPrimerParcial/Code/MainBank.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <windows.h>
+#include <iostream>
+#include "MenuBank.h"
+using namespace std;
+
+int main() {
+	MenuBank objmenu(1);
+	objmenu.menuOptions();
+	system("pause");
+}
+
+
